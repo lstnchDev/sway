fd3bdfc4f96077a3985ffe7f2abd5f8ddcfa727b
lstnchDev <freest2334@gmail.com>
2022-10-07 00:29:46 +0300

    adaptive ref

--- js/script.js
+++ js/script.js
@@ -5,58 +5,136 @@ const btnNext = document.querySelector(".btn_next")
 const btnPrev = document.querySelector(".btn_prev")
 
 const carouselItem = document.querySelector('.carousel-item')
-const removeItem = document.querySelector('.review-item')
+const reviewItem = document.querySelector('.review-item')
+const galleryItem = document.querySelector('.gallery_item')
 
-let marginBase = 0
-let widthReview = removeItem.offsetWidth+30
-let maxWidth = -carouselItem.offsetWidth
+const btnBurger =  document.querySelector(".menu-btn")
+const menuBurger = document.querySelector(".menu-burger")
+
+const menuSelected = document.querySelectorAll(".menu-selected")
+
+const btnCart = document.querySelector(".btnCart")
+const modalCart = document.querySelector(".modal-cart")
+const modalContent = document.querySelector(".modal-content")
+
+const btnCloseCart = document.querySelector(".close")
+
+const btnSearch = document.querySelector(".searchIcon")
+const searchInput = document.querySelector(".search-input")
+const headerMenu = document.querySelector(".header-top-menu")
+
+let lastTarget = menuSelected[0]
 
 
-const nextSlide = ()=>{
-    // marginBase += widthReview
-    console.log(carouselItem.offsetWidth)
 
-    marginBase = marginBase - widthReview <= maxWidth? maxWidth : marginBase - widthReview
-    console.log(maxWidth)
-    document.getElementById("gallery-item").style.marginLeft = `${marginBase}px`
-    console.log(document.getElementById("gallery-item").style.marginLeft)
+btnSearch.addEventListener('click', ()=>{
+    searchInput.style.display = 'block'
+    headerMenu.style.gridArea = "2 / 1 / 3 / 6"
+})
 
-}   
-const prevSlide = ()=>{
-    marginBase = marginBase + widthReview <= 0 ? marginBase + widthReview : 0 
-    // marginBase += widthReview
+btnCloseCart.addEventListener('click', ()=> {
+    modalCart.style.display = "none"
+    modalContent.style.display = "none"
+})
+btnCart.addEventListener('click', ()=> {
+    modalCart.style.display = "block"
+    modalContent.style.display = "block"
+})
 
-    document.getElementById("gallery-item").style.marginLeft = `${marginBase}px`
-    console.log(document.getElementById("gallery-item").style.marginLeft)
+modalCart.addEventListener('click', ()=> {
+    modalCart.style.display = "none"
+    modalContent.style.display = "none"
+})
+btnBurger.addEventListener('click', ()=>{
+    menuBurger.classList.toggle('active')
+})
 
-}   
-
-btnNext.addEventListener('click', nextSlide)
-btnPrev.addEventListener('click', prevSlide)
-
-for (let btn of dropBtn){
-    let activeBtn
-    btn.addEventListener('click', (e)=>{
-       
-        if (btn.classList.contains("dropdown-active") === true) {
-            
-            btn.classList.remove("dropdown-active")
-            e.target.nextElementSibling.classList.remove("show")
-        }else {
-            for (let button of dropBtn){
-                if (button.classList.contains("dropdown-active") === true) {
-                    button.classList.remove("dropdown-active")
-                    button.nextElementSibling.classList.remove("show")
-                }
-            }
-            e.target.nextElementSibling.classList.toggle("show")
-            btn.classList.toggle("dropdown-active")  
-    
-        }
+for (menu of menuSelected){
+    menu.addEventListener("click", (e)=>{
+        e.preventDefault()
+        console.log(e.target)
+        lastTarget.classList.remove("active-menu")
+        e.target.classList.toggle("active-menu")
+        lastTarget = e.target
     })
 }
 
-window.onclick = (event)=>{    
+class Slider{
+    
+    constructor (name, galleryItem, carouselItem, reviewItem){
+        this.galleryItem = galleryItem
+        this.carouselItem = carouselItem
+        this.reviewItem = reviewItem
+        this.marginBase = 0
+        this.scrollCount = 0
+        this.name = name
+        this.widthReview = reviewItem.offsetWidth+30
+        this.maxWidth = ((galleryItem.offsetWidth/carouselItem.offsetWidth)+1)>6 ? 5 : (galleryItem.offsetWidth/carouselItem.offsetWidth)+1
+    }
+
+    nextBtn(){
+        console.log(this.name)
+        if (this.scrollCount<this.maxWidth) {
+            this.marginBase = this.marginBase-this.widthReview
+            this.scrollCount++
+        }  
+        galleryItem.style.marginLeft = `${this.marginBase}px`
+    }
+    prevBtn(){
+        if (this.scrollCount>0) {
+            this.marginBase = this.marginBase+this.widthReview
+            this.scrollCount--
+        } 
+        galleryItem.style.marginLeft = `${this.marginBase}px`
+    }
+}
+const sliderReview = new Slider(123, galleryItem, carouselItem, reviewItem)
+const sliderMove = (scrollCount, maxWidth, itemScroll, booleanSide)=>{
+    if (booleanSide){
+        if (scrollCount<maxWidth) {
+            marginBase = marginBase-widthReview
+            scrollCount++
+        }  
+        itemScroll.style.marginLeft = `${marginBase}px`    
+    }else {
+        if (scrollCount>0) {
+            marginBase = marginBase+widthReview
+            scrollCount--
+        } 
+        itemScroll.style.marginLeft = `${marginBase}px`
+    }
+}   
+btnNext.addEventListener('click', () => sliderReview.nextBtn())
+btnPrev.addEventListener('click', () => sliderReview.prevBtn())
+
+
+const addDropClass = (dropBtn)=>{
+    for (let btn of dropBtn){
+        btn.addEventListener('click', (e)=>{
+           e.preventDefault()
+            if (btn.classList.contains("dropdown-active") === true) {
+                
+                btn.classList.remove("dropdown-active")
+                e.target.nextElementSibling.classList.remove("show")
+            }else {
+                for (let button of dropBtn){
+                    if (button.classList.contains("dropdown-active") === true) {
+                        button.classList.remove("dropdown-active")
+                        button.nextElementSibling.classList.remove("show")
+                    }
+                }
+                e.target.nextElementSibling.classList.toggle("show")
+                btn.classList.toggle("dropdown-active")  
+        
+            }
+        })
+    }
+}
+addDropClass(dropBtn)  
+
+
+
+window.onclick = (event)=>{  
     if (!event.target.matches('.dropbtn') && !event.target.matches('.dropdown-content')){
         let dropdowns = document.getElementsByClassName("dropdown-content");
         for (let i = 0; i<dropdowns.length; i++){
@@ -65,4 +143,8 @@ window.onclick = (event)=>{
         
         }
     }
+    if (!event.target.matches('.menu-burger') && !event.target.matches('.menu-btn') && !event.target.matches('.btnBurger')){
+        menuBurger.classList.remove('active')
+    }
+    
 }
